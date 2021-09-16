34124cfc5266a87e83016ec294567d2dfb5e1b79
chris botelho <chris.botelho29@gmail.com>
2021-09-16 10:10:58 -0400

    added images and ability to flip through them. remember to add constraints :)

--- you are awesome! v2/ViewController.swift
+++ you are awesome! v2/ViewController.swift
@@ -9,15 +9,44 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    
+   var imageNumber = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("viewDidLoad has run!")
     }
 
     @IBAction func messageButtonPressed(_ sender: UIButton) {
-        print("🦩The message button was pressed!🦩")
-        messageLabel.text = "you are awesome!"
+
+        print(imageNumber)
+        // let imageName = "image" + String(imageNumber)
+        let imageName = "image\(imageNumber)"
+        imageView.image = UIImage(named: imageName)
+        imageNumber = imageNumber + 1
+        if imageNumber == 10 {
+            imageNumber = 0
+        }
+        
+          
+
+        //        let awesomeMessage = "you are awesome!"
+        //        let fabMessage = "you are FABULOUS!"
+        //        let greatMessage = "you are great!!"
+        //
+        //        print("🦩The message button was pressed!🦩")
+        //
+        //        if messageLabel.text == awesomeMessage {
+        //            messageLabel.text = fabMessage
+        //            imageView.image = UIImage(named: "image1")
+        //        } else if messageLabel.text == fabMessage {
+        //            messageLabel.text = greatMessage
+        //            imageView.image = UIImage(named: "image0")
+        //        } else {
+        //            messageLabel.text = awesomeMessage
+        //            imageView.image = UIImage(named: "image4")
+        //        }
+        //
     }
     
 }
